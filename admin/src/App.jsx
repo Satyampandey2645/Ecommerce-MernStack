582ed8e1f27a2eb83b89a67ea1f73c86c11b5198
Satyampandey2645 <satyamp1303@gmail.com>
2024-05-03 22:18:37 +0530

    Mern

---
+++ admin/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from './Components/Navbar/Navbar'
+import Admin from './Pages/Admin/Admin'
+import AddProduct from './Components/AddProduct/AddProduct'
+import Sidebar from './Components/Sidebar/Sidebar'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Admin/> 
+    </div>
+  )
+}
+
+export default App
